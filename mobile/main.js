c1cdf79a6dc706f847612b49cef850ee86bc155c
Desenvolvimento15 <desenvolvimento15@dl.com.br>
2025-10-20 14:44:39 -0300

    Vs01

---
+++ mobile/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".slick-end").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+});
+
